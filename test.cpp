99b69546be23ffa09f747e5919bf05d5d98ab12a
captainkong <kong-peng@qq.com>
2020-01-09 11:57:18 +0800

    test add i2s

--- test.cpp
+++ test.cpp
@@ -1,19 +1,34 @@
 #include <iostream>
-#define	n 	5
 using namespace std;
+
+//int 转 string
+int intToString(int n,string *str)
+{
+	int tem;
+	while(n){
+		tem=n%10;
+		n=n/10;
+		string s(1,'0'+tem);
+		*str=s+*str;
+	}
+	return str->length();
+}
 
 int main(int argc, char const *argv[])
 {
+	int x=12345;
 	
-	int sum=0;
-	for (int i = 1; i <= n; i-=2)
+	string str,ss;
+	//ss=to_string(x);
+	int n=intToString(x,&str);
+	for (int i = 0; i < n; i++)
 	{
-		for(int j=0;j<n-i/2;j++)
-			cout<<" ";
-		for(int k=0;k<i;k++)
-			cout <<"*";
-		cout <<endl;
+		cout <<str[i]<<endl;
 	}
 	
+	
+
+	//std::cout <<"toString:"<<str<<endl;
+	//std::cout <<"to_string:"<<ss<<endl;
 	return 0;
 }
