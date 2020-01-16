60a867632f00ecfc658143c4029d73192fea96d1
captainkong <kong-peng@qq.com>
2020-01-16 15:24:48 +0800

    add new file '201703-2.cpp'

--- test.cpp
+++ test.cpp
@@ -2,31 +2,30 @@
 using namespace std;
 
 //int 转 string
-int intToString(int n,string *str)
+int intToString(int n, string *str)
 {
 	int tem;
-	while(n){
-		tem=n%10;
-		n=n/10;
-		string s(1,'0'+tem);
-		*str=s+*str;
+	while (n)
+	{
+		tem = n % 10;
+		n = n / 10;
+		string s(1, '0' + tem);
+		*str = s + *str;
 	}
 	return str->length();
 }
 
 int main(int argc, char const *argv[])
 {
-	int x=12345;
-	
-	string str,ss;
+	int x = 12345;
+
+	string str, ss;
 	//ss=to_string(x);
-	int n=intToString(x,&str);
+	int n = intToString(x, &str);
 	for (int i = 0; i < n; i++)
 	{
-		cout <<str[i]<<endl;
+		cout << str[i] << endl;
 	}
-	
-	
 
 	//std::cout <<"toString:"<<str<<endl;
 	//std::cout <<"to_string:"<<ss<<endl;
